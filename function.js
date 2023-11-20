5befc9b28067441aefc4eb563d3fd02064a8ae49
Amimul Ihsan Zubair <86657812+aizubair21@users.noreply.github.com>
2023-11-21 01:55:01 +0600

    Add Sidebar
    -> design update
    -> a boiler template design appear on the exam screen on first loaded

--- function.js
+++ function.js
@@ -2,9 +2,11 @@
 const leftTabNav = document.querySelectorAll('.tab_item');
 const leftTabContent = document.querySelectorAll('.tab_content');
 const tna = document.getElementsByClassName('tab_item_active')[0];
-// console.log(tna);
+// console.log(tna)
 
 
+
+//left tab and navs
 leftTabContent[4].style.display = 'block';
 leftTabNav[4].classList.add('tab_item_active')
 leftTabNav.forEach((lnav, lindex) => {
--- mcq.js
+++ mcq.js
@@ -171,8 +171,8 @@ const disabledOptinInput = document.getElementById("optionDisabled");
 
 const totalQuestion = data.length;
 let currentQuestionIndex = 0;
-let displayQuestionIndex = 0;
-let questionLimitation = 25;
+let displayQuestionIndex = 10;
+let questionLimitation = 10;
 let totalTime = 10;
 let secound = 60;
 let isExamStarted = false;
@@ -438,8 +438,54 @@ function submitExam() {
     <div class="exam_finished_box">
         <button class="fns_btn">Show Result</button>
         <div class="info_text">
-            *To start new exam refresh your webpage. <span style="cursor:pointer; padding:5px; margin-left:5px; border:1px solid white" onclick="window.location.reload()">New</span>
-        </div>
+            
+        <style>
+            .button-reload {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 6px 12px;
+            gap: 8px;
+            height: 34px;
+            width: 112px;
+            border: none;
+            background: #ff362b34;
+            border-radius: 20px;
+            cursor: pointer;
+            }
+
+            .lable {
+            line-height: 20px;
+            font-size: 17px;
+            color: #fff;
+            font-family: sans-serif;
+            letter-spacing: 1px;
+            }
+
+            .button-reload:hover {
+            background: #ff362b52;
+            }
+
+            .button-reload:hover .svg-icon {
+            animation: spin 2s linear infinite;
+            }
+
+            @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(-360deg);
+            }
+            }
+        </style>
+
+        <button class="button-reload" onclick="window.location.reload()">
+        <svg class="svg-icon" fill="none" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><g stroke="#fff" stroke-linecap="round" stroke-width="1.5"><path d="m3.33337 10.8333c0 3.6819 2.98477 6.6667 6.66663 6.6667 3.682 0 6.6667-2.9848 6.6667-6.6667 0-3.68188-2.9847-6.66664-6.6667-6.66664-1.29938 0-2.51191.37174-3.5371 1.01468"></path><path d="m7.69867 1.58163-1.44987 3.28435c-.18587.42104.00478.91303.42582 1.0989l3.28438 1.44986"></path></g></svg>
+        <span class="lable">Repeat</span>
+      </button>
+            </div>
     </div>
 
     `;
@@ -515,5 +561,3 @@ setInterval(() => {
         document.getElementsByClassName('exam_layout')[0].classList.add('exam_layout_2');
     }
 }, 100);
-
-
